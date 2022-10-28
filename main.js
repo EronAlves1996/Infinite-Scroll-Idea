8353d41f3d14d6dc2cafd65d35d54c78624dbaa2
Eron Alves <eron.alves@rocketmail.com>
2022-10-27 22:41:56 -0300

    Little refactoring

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 import { Queue, Blogpost } from "./queue.mjs";
 
 const queue = new Queue();
-const articles =  [];
 const dock = document.querySelector("#dock");
 
 fetch("https://dev.to/api/articles?username=tttecnology")
   .then(response => response.json())
   .then(json => json.map(n => n.slug)
-    .forEach(article => articles.push(article)))
-  .then(n=> articles.forEach(article => fetch("https://dev.to/api/articles/tttecnology/" + article)
+    .forEach(article => fetch("https://dev.to/api/articles/tttecnology/" + article)
     .then(response => response.json())
     .then(article =>
       queue.enqueue(new Blogpost(article.title, article.body_html)))))
